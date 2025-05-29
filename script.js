308cd7f9d4c2742d10e8f518cfe1f3d999dc7794
stephangreiner <49695489+stephangreiner@users.noreply.github.com>
2025-05-29 07:19:55 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let g1_1 = "";
-let g1_2 = "";
-let g1_3 = "";
+for (let i = 1; i <= 17; i++) {
+  window[`g1_${i}`] = "";
+}
+
 let greekLoaded = false;
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -26,34 +27,102 @@ function tg1_1() {
     console.warn("Greek text not loaded yet.");
     return;
   }
-
-  const e = document.getElementById("x1_1");
+const e = document.getElementById("x1_1");
   e.innerHTML = e.innerHTML.trim() !== "" ? "" : g1_1;
 }
 
 function tg1_2() {
-  if (!greekLoaded) {
-    console.warn("Greek text not loaded yet.");
-    return;
-  }
-
-  const e = document.getElementById("x1_2");
+ const e = document.getElementById("x1_2");
   e.innerHTML = e.innerHTML.trim() !== "" ? "" : g1_2;
 }
 
 function tg1_3() {
-  if (!greekLoaded) {
-    console.warn("Greek text not loaded yet.");
-    return;
-  }
-
-  const e = document.getElementById("x1_3");
-  e.innerHTML = e.innerHTML.trim() !== "" ? "" : g1_3;
+const e = document.getElementById("x1_3");
+e.innerHTML = e.innerHTML.trim() !== "" ? "" : g1_3;
 }
+
+function tg1_4() {
+ const e = document.getElementById("x1_4");
+  e.innerHTML = e.innerHTML.trim() !== "" ? "" : g1_4;
+}
+
+function tg1_5() {
+const e = document.getElementById("x1_5");
+e.innerHTML = e.innerHTML.trim() !== "" ? "" : g1_5;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function de_an_aus() {
   const elements = document.querySelectorAll(".de");
   elements.forEach(element => {
-    element.style.display = (element.style.display === "none") ? "block" : "none";
+   
   });
 }
+
+function b1ausblenden(){
+  const e = document.getElementById ("Buch1")
+  e.style.display = (e.style.display === "none") ? "block" : "none";
+  };
+
+  function b2ausblenden(){
+  const e = document.getElementById ("Buch2")
+  e.style.display = (e.style.display === "none") ? "block" : "none";
+  };
+
+function b3ausblenden(){
+  const e = document.getElementById ("Buch3")
+  e.style.display = (e.style.display === "none") ? "block" : "none";
+  };
+
+  function b4ausblenden(){
+  const e = document.getElementById ("Buch4")
+  e.style.display = (e.style.display === "none") ? "block" : "none";
+  };
+
+  function b5ausblenden(){
+  const e = document.getElementById ("Buch5")
+  e.style.display = (e.style.display === "none") ? "block" : "none";
+  };
+
+  function b6ausblenden(){
+  const e = document.getElementById ("Buch6")
+  e.style.display = (e.style.display === "none") ? "block" : "none";
+  };
+  
+
